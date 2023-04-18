f1d240f8822fef4739e3575e563283d6652fce8d
Agnieszka Ciesińska <agnieszkaciesinska@gmail.com>
2023-04-18 22:27:47 +0200

    Add class active to the clicked link

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 'use strict';
 
 function titleClickHandler(event){
-    console.log('Link was clicked!');
-    console.log(event);
+  const clickedElement = this;
+  console.log('Link was clicked!');
   
-    /* remove class 'active' from all article links  */
+    /* [DONE] remove class 'active' from all article links  */
 
 const activeLinks = document.querySelectorAll('.titles a.active');
 
@@ -12,14 +12,18 @@ for(let activeLink of activeLinks){
   activeLink.classList.remove('active');
 }
   
-    /* add class 'active' to the clicked link */
+    /* [IN PROGRESS] add class 'active' to the clicked link */
+
+console.log('clickedElement:', clickedElement);
+clickedElement.classList.add('active');
+
   
-    /* remove class 'active' from all articles */
+    /* [DONE] remove class 'active' from all articles */
 
 const activeArticles = document.querySelectorAll('.posts .active');
 
 for(let activeArticle of activeArticles){
-    activeArticle.classList.remove('active');
+  activeArticle.classList.remove('active');
     }
   
     /* get 'href' attribute from the clicked link */
